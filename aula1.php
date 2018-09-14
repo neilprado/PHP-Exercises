baabaf4fe07a1ea73f16573aaa33a05d9febcf4f
neilprado <neiljohn17@gmail.com>
2018-09-13 23:40:16 -0300

    Adicionando Materialize

--- aula1.php
+++ aula1.php
@@ -4,6 +4,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
   <title>Document</title>
 </head>
 <body>
@@ -11,7 +12,7 @@
     $clientes = [array('nome' => 'Edemberg', 'endereco' => 'Rua A', 'cpf'=> '1', 'cidade'=> 'JPA'),
                 array('nome' => 'Tetéio', 'endereco' => 'Rua B', 'cpf' => '2', 'cidade' => 'CG')];
   ?>
-  <table>
+  <table class="highlight responsive-table">
     <thead>
       <tr>
         <?php
