242b7c999ca23f06057bbef33acaee4b1ca19d46
Neil John Ávila Prado Júnior <20171370045@ad.ifpb.edu.br>
2018-10-22 14:52:04 -0300

    Exemplo composer

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+            use App\Model\Midia\DVD;
+            include_once 'vendor/autoload.php';
+            $dvd = new DVD('1', 'Ana ao Vivo',time());
+            echo 'Nome:'.$dvd->getNome();
+        ?>
+    </body>
+</html>
