4fcb113c50f0e8e5e68d92cbab44c7af338f5a31
neilprado <neiljohn17@gmail.com>
2018-11-11 14:53:58 -0300

    exercicio PWEB de formulário com select múltiplo

---
+++ Forms/SelectMult/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Document</title>
+</head>
+<body>
+  <form action="multi.php" method="get">
+    <select name="genero[]" multiple>
+      <?php
+        include_once 'multi.php';
+        $chaves = array_keys($filmes);
+        foreach($chaves as $v){?>
+          <option value="<?php echo $v; ?>"> <?php echo $v;?> </option>
+        <?php } ?>
+    </select>
+    <input type="submit">
+  </form>
+</body>
+</html>
