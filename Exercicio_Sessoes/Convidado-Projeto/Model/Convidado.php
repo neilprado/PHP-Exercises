42e877001ff0b2ce1b412f9e3072da6a14738733
neilprado <neiljohn17@gmail.com>
2018-12-03 14:34:53 -0300

    Add files via upload

---
+++ Exercicio_Sessoes/Convidado-Projeto/Model/Convidado.php
@@ -0,0 +1,35 @@
+<?php
+    namespace App\Model;
+
+    class Convidado
+    {
+        private $nome;
+        private $sexo;
+
+        function __construct($nome, $sexo)
+        {
+            $thus->nome = $nome;
+            $this->sexo = $sexo;
+        }
+
+        function getNome()
+        {
+            return $this->nome;
+        }
+
+        function getSexo()
+        {
+            return $this->sexo;
+        }
+
+        function setNome($nome)
+        {
+            $this->nome = $nome;
+        }
+
+        function setSexo($sexo)
+        {
+            $this->sexo = $sexo;
+        }
+    }
+?>
