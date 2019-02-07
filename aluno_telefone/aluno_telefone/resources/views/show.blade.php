6f2a35239dff19b9820cd7908d2e806ee28ac358
neilprado <neiljohn17@gmail.com>
2019-02-07 16:55:58 -0300

    Finishing exercises

--- aluno_telefone/aluno_telefone/resources/views/show.blade.php
+++ aluno_telefone/aluno_telefone/resources/views/show.blade.php
@@ -3,7 +3,7 @@
     <div class="card">
         <p>Nome: {{$aluno->nome}}</p>
         <p>Matricula: {{$aluno->matricula}}</p>
-        @forelse($aluno->telefones() as $t)
+        @forelse($aluno->telefones()->get() as $t)
             <p>Telefone: {{$t->numero}}</p>
         @empty
             <p>Não há telefones cadastrados</p>
